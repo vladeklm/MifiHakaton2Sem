9bd2bb5d13bad1c875bf42c13a433b15192a1ef5
leralee <lerakenobi@gmail.com>
2025-05-04 17:37:10 +0300

    изменен эндпоинт transactions -> operations

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
-        ? 'http://localhost:8080/api/v1/transactions'
+        ? 'http://localhost:8080/api/v1/operations'
         : '/api/v1/transactions',
   headers: {
     'Content-Type': 'application/json'
