77667659335c5664e4a22bdb18659a6128faf4bf
Peter <p00kn98kt79k@mail.ru>
2025-05-06 22:10:54 +0300

    Исправил AuthenticationDTO теперь обязательные поля имя и фамилия

--- backend/finaudit/src/main/java/it/globus/finaudit/entity/Client.java
+++ backend/finaudit/src/main/java/it/globus/finaudit/entity/Client.java
@@ -27,8 +27,10 @@ public class Client {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Column(nullable = false)
     private String firstName;
 
+    @Column(nullable = false)
     private String secondName;
 
     private String patronymic;
